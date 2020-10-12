6350ea9d8126f6d07a366743d2b4c8d70cb21845
chiamsimon17 <chiamsimon17@gmail.com>
2020-10-12 00:02:03 -0700

    Initial commit

---
+++ app/src/main/java/com/tasker/greenmark/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tasker.greenmark.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tasker.greenmark.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
